5ad6b8790049443242bd593fa1c5937b54feb4f3
Rayssa Alves <rayssa.a.vieira@outlook.com>
2022-01-10 15:55:21 +0100

    DB configuration

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,8 @@
-import express, { request, response } from "express";
+import "reflect-metadata";
+import express from "express";
 
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) =>{
-    //request => entrando
-    //response => saindo
-    return response.send("Hello")
-})
-
-app.post("/test-post",(request, response) =>{
-    return response.send("Hello metodo post")
-} )
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running yaa"));
